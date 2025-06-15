c1e1b9414f468226bd6f2dd87f591ba8679aca8c
Abhijeet-2005 <abhijitzingare728@gmail.com>
2025-06-15 22:04:50 +0530

    calculator program using variables and operators
    This code takes two inputs and performs +, -, *, / operations.

---
+++ 01_Variables_operators.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main() {
+  int a,b;
+  cout<<"Enter your first number:"<<endl;
+  cin>>a;
+  cout<<"Enter your second number:"<<endl;
+  cin>>b;
+  cout<<"Addition:"<<(a+b)<<endl;
+  cout<<"Subtraction:"<<(a-b)<<endl;
+  cout<<"Multiplication:"<<(a*b)<<endl;
+  cout<<"Division:"<<(a/b)<<endl;
+
+  return 0;
+}
